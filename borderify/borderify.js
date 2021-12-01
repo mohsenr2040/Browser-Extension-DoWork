1681f84c6499931cb66ff33c7c8d51782a750edf
mohsen rezaei <mohsenr2020@gmail.com>
2021-12-02 00:47:19 +0330

    new changes

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,31 +1,57 @@
 //document.body.style.border = "15px solid red";
 
 
-var file = './mydata.txt';
-var reader = new FileReader();
-reader.onload = function(progressEvent){    
-  var fileContentArray = this.result.split(/\r\n|\n/);
-  for(var line = 0; line < lines.length-1; line++){
-    console.log(line + " --> "+ lines[line]);
-  }
+var lines= new Array();
 
-
-
-
-
-
-//var checkboxes = document.querySelectorAll('input[type=checkbox]')
 var grid = document.getElementById("Table1");
-var checkboxes = grid.querySelectorAll('input[type=checkbox]')
+var newRow = grid.insertRow(0);
 
+// Insert a cell at the end of the row
+var newCell = newRow.insertCell();
+var button = document.createElement('input');
+button.type = 'file';
+button.id = 'file';
+button.name = 'file';
+button.onchange=function()
+{
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+    // Entire file
+    console.log(this.result);
+
+    // By lines
+     lines = this.result.split('\n');
+    for(var line = 0; line < lines.length; line++){
+          alert(lines[line]);
+    }
+  };
+  reader.readAsText(file);
+
+  var checkboxes = grid.querySelectorAll('input[type=checkbox]')
+alert("1");
 for (var i = 0; i < checkboxes.length; i++) {
     var row = checkboxes[i].parentNode.parentNode;
-    if(row.cells[3].innerHTML=="India")
+    alert(lines[0]);
+    if(lines[0]=="France")
         {
           checkboxes[i].checked = true;
 
-         }
+        }
 
   
   //array.push(checkboxes[i].value)
+      }
 }
+newCell.appendChild(button);
+
+
+
+
+
+
+//var items = document.getElementsByName('acs');
+//for (var i = 0; i < items.length; i++) {
+//    if (items[i].type == 'checkbox')
+//        items[i].checked = true;
